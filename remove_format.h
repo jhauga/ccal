cefdf2641eccafcc098cdf842f28137fc217c858
jhauga <johnhaugabook@gmail.com>
2025-06-23 19:57:49 -0400

    move: remove_comma to remove_format

---
+++ remove_format.h
@@ -0,0 +1,10 @@
+// remove_format.h
+// Remove all formatting from a string, modifying it in-place
+
+#ifndef REMOVE_FORMAT_H
+#define REMOVE_FORMAT_H
+
+// Forward declaration
+void remove_format(char* str);
+
+#endif
